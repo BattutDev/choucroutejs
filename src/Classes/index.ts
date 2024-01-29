008d50e42df687c0f409e7de5dc2f4cce0947b98
BattutDev <antoine@battut.dev>
2024-01-29 23:17:44 +0100

    feat: Add middlewares

--- src/Classes/index.ts
+++ src/Classes/index.ts
@@ -1,7 +1,8 @@
 import Server from './server';
 import Request from './Request';
+import BaseMiddleware from './BaseMiddleware';
 
-export { Server, Request };
+export { Server, Request, BaseMiddleware };
 
 export * from './types';
 
--- src/Classes/server.ts
+++ src/Classes/server.ts
@@ -1,5 +1,5 @@
 import http, {createServer} from 'http';
-import {CallBackType, Method, Request} from './';
+import {CallBackType, Method, Request, BaseMiddleware} from './';
 
 export default class Server {
 
@@ -40,16 +40,25 @@ export default class Server {
 		this.request(route, Method.DELETE, callback);
 	}
 
-	public request<T> (route: string, method: Method, callback: CallBackType<T>) {
+	public request<T> (route: string, method: Method, callback: CallBackType<T>, middlewares: Array<BaseMiddleware> = []) {
 		this.getListener().on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
 			if (req.url === route && req.method === method) {
 				new Request()
 					.setHeaders(req.headers)
 					.setMethod(method)
 					.setBody(req).then((request) => {
-						const response = callback(request, res);
-						res.writeHead(200, {'Content-Type': 'application/json'});
-						res.end(JSON.stringify(response));
+
+						const next = () => {
+							const response = callback(request, res);
+							res.writeHead(200, {'Content-Type': 'application/json'});
+							res.end(JSON.stringify(response));
+						};
+
+						if (middlewares.length > 0) {
+							middlewares[0].run(request, res, next);
+						} else {
+							next();
+						}
 					});
 			}
 		});
